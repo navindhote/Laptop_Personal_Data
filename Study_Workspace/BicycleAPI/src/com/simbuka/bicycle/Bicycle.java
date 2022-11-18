caf4675d7ab0faf162a4b0359822ef92a013212b
Navin Ajit Dhote <navin.dhote214@gmail.com>
2022-11-18 16:51:20 +0200

    Add files via upload

---
+++ Study_Workspace/BicycleAPI/src/com/simbuka/bicycle/Bicycle.java
@@ -0,0 +1,27 @@
+package com.simbuka.bicycle;
+
+public class Bicycle {
+	
+	private int gear=0;
+	
+	public void increaseGear()
+	{
+		gear++;
+	}
+	
+	public void decreaseGear()
+	{
+		gear--;
+	}
+	
+	public float cycle(int number_of_rotations)
+	{
+		return 0.0f;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
